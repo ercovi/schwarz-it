1bac1ab3cb5a14c077ae52e711c04af1cf9ba689
Èric Corbí <eric@andreullos.com>
2025-02-22 15:28:56 +0100

    ADD: Eslint and tailwind configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const isDev = process.env.APP_ENV == 'development'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-11-01',
+    modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+    css: ['@/assets/styles/global.css'],
+    dev: isDev,
+    devtools: { enabled: isDev },
 })
