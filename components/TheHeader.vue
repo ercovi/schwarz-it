3268e60632c59243e4bd25c392c66d0c8baa24c4
Èric Corbí <eric@andreullos.com>
2025-02-24 21:54:16 +0100

    ADD: Nav in header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,7 +1,9 @@
 <template>
-    <header class="sticky top-0 z-10 flex items-center justify-center w-full h-16 bg-white border-b border-gray-300 gap-x-8">
-        <NuxtLink to="/" class="text-xl font-bold">Home</NuxtLink>
-        <NuxtLink to="/" class="text-xl font-bold">Cards</NuxtLink>
-        <NuxtLink to="/" class="text-xl font-bold">Cards</NuxtLink>
+    <header class="sticky top-0 z-10 flex items-center justify-center w-full h-16 bg-white border-b border-gray-300">
+        <nav class="flex gap-x-8">
+            <NuxtLink to="/" class="text-xl font-bold">Home</NuxtLink>
+            <NuxtLink to="/" class="text-xl font-bold">Cards</NuxtLink>
+            <NuxtLink to="/" class="text-xl font-bold">Cards</NuxtLink>
+        </nav>
     </header>
 </template>
