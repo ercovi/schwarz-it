83bf3a460c23dfcb3f24030662e666d6a5dfa181
Èric Corbí <eric@andreullos.com>
2025-02-25 19:00:08 +0100

    ADD Èric's link info to footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,7 @@
 <template>
-    <footer>
-        <p>Footer</p>
+    <footer class="w-full py-4 text-center">
+        <p class="text-sm italic text-gray-400">
+            Developed by <a href="https://ercovi.dev" target="_blank">Èric Corbí</a>
+        </p>
     </footer>
 </template>
