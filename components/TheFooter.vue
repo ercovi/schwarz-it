0d5e0f8537bc8ea64c65e649ac649df4c6404b6e
Èric Corbí <eric@andreullos.com>
2025-02-26 00:33:44 +0100

    ADD Github repo to footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,7 +1,8 @@
 <template>
-    <footer class="w-full py-4 text-center">
-        <p class="text-sm italic text-gray-400">
+    <footer class="w-full py-4 text-sm italic text-center text-gray-400">
+        <p >
             Developed by <a href="https://ercovi.dev" target="_blank">Èric Corbí</a>
         </p>
+        <p>Public repository link <a class="underline" href="https://github.com/ercovi/schwarz-it" target="_blank">here</a></p>
     </footer>
 </template>
