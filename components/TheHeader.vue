952a28b6c1cdf7eada75e69be0e5c98398e3fb2d
Èric Corbí <eric@andreullos.com>
2025-02-25 19:06:35 +0100

    FIX: Canched menu to ref and iterate items to avoid repeating code

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,8 +1,24 @@
 <template>
     <header class="sticky top-0 z-10 flex items-center justify-center w-full h-16 bg-white border-b border-gray-300">
-        <nav class="flex gap-x-8">
-            <NuxtLink to="/" class="text-xl font-bold">Home</NuxtLink>
-            <NuxtLink to="/collection" class="text-xl font-bold">Collection</NuxtLink>
+        <nav 
+            v-if="menuItems.length > 0"
+            class="flex gap-x-8"
+        >
+            <NuxtLink
+                v-for="{name, path, id} in menuItems"
+                :key="id"
+                :to="path"
+                class="text-xl font-bold"
+            >
+                {{name}}
+            </NuxtLink>
         </nav>
     </header>
 </template>
+
+<script setup>
+const menuItems = ref([
+    { name: 'Home', path: '/', id: useId() },
+    { name: 'Collection', path: '/collection', id: useId() },
+]);
+</script>
