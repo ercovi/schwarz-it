c11ae0844b115a16a30a099d587f61dab848a5af
Èric Corbí <eric@andreullos.com>
2025-02-26 18:26:15 +0100

    ADD: Accessibility details to heart button

--- components/Card/Icons/Heart.vue
+++ components/Card/Icons/Heart.vue
@@ -1,23 +1,27 @@
 <template>
-    <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        class="transition-all duration-300 scale-100 hover:scale-125"
-        :class="{
-            'fill-red-500 hover:fill-red-200': isInCollection,
-            'hidden fill-transparent hover:fill-red-500': !isInCollection,
-        }"
-        stroke="black"
-        stroke-width="2" 
-        stroke-linecap="round"
-        stroke-linejoin="round"
+    <button
+        :aria-label="label"
         @click="toggleCardInCollection"
     >
-        <path 
-            d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
-        />
-    </svg>
+        <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            class="transition-all duration-300 scale-100 hover:scale-125"
+            :class="{
+                'fill-red-500 hover:fill-red-200': isInCollection,
+                'hidden fill-transparent hover:fill-red-500': !isInCollection,
+            }"
+            stroke="black"
+            stroke-width="2" 
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path 
+                d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
+            />
+        </svg>
+    </button>
 </template>
 
 <script setup>
@@ -34,4 +38,10 @@ const isInCollection = computed(() => collectionStore.isInCollection(props.card.
 const toggleCardInCollection = () => {
     collectionStore.toggleCardInCollection(props.card);
 };
+
+const label = computed(() => 
+    isInCollection.value 
+        ? `Remove ${props.card?.name} from your collection` 
+        : `Add ${props.card?.name} to your collection`
+);
 </script>
