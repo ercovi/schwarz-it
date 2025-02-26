75864dbf7ec0cab401539310c857f03827e2db0d
Èric Corbí <eric@andreullos.com>
2025-02-26 19:54:06 +0100

    ADD: useHead to home page

--- pages/index.vue
+++ pages/index.vue
@@ -12,11 +12,22 @@ watch(data, (newData) => {
     cardsStore.pushCards(newData.cards)
   }
 }, { immediate: true });
+
+useHead({
+    title: `Cards Home Page - Page ${page.value}`,
+    meta: [
+        { name: 'description', content: 'Explore the cards and collect them.' },
+        { name: 'robots', content: 'index, follow' },
+        { property: 'og:title', content: `Cards - Page ${page.value}` },
+        { property: 'og:description', content: 'Explore the cards and collect them.' },
+        { property: 'og:image', content: '/card.webp' },
+    ]
+})
 </script>
 
 <template>
     <section class="flex flex-col gap-y-4 md:gap-y-8">
-        <CommonLoader v-if="pending"  />
+        <CommonLoader v-if="pending" />
         <div v-if="error">
             Failed to load cards. 
             <CommonButton 
