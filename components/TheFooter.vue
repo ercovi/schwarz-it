bc947cd30b6a0b9a0728b05125860bfb2d80d3f2
Èric Corbí <eric@andreullos.com>
2025-02-27 01:57:32 +0100

    FIX: footer Descriptive link text

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,6 +3,6 @@
         <p >
             Developed by <a href="https://ercovi.dev" aria-label="View the developer portfolio" rel="noopener noreferrer" target="_blank">Èric Corbí</a>
         </p>
-        <p>Public repository link <a class="underline" href="https://github.com/ercovi/schwarz-it" aria-label="View the public repository on GitHub" target="_blank">here</a></p>
+        <p>Public repository link <a class="underline" href="https://github.com/ercovi/schwarz-it" aria-label="View the public repository on GitHub" target="_blank">in this github link</a></p>
     </footer>
 </template>
